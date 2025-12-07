9f1085c08cbb13c09e5e90198967d84f5f1a0075
Vivek Singh <vivekjnv82@gmail.com>
2025-12-07 22:24:59 +0530

    make changes in packages

--- odyssey-travel-client/src/App.jsx
+++ odyssey-travel-client/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 
-import LandingPage from './pages/LandingPage/LandingPage'
+import LandingPage from './pages/public/LandingPage/LandingPage'
 
 function App() {
 
   return (
     <Routes>
-      <Route path='/' element={<LandingPage />}/>
+      <Route path='/' element={<LandingPage />} />
+      <Route path='login' element={<LandingPage />} />
+      <Route path='register' element={<LandingPage />} />
     </Routes>
   )
 }
