52e4bd8b481730b9bf6117bcb22fbb73ab1530c8
Amruta <patilamrutak2003@gmail.com>
2026-01-24 18:25:39 +0530

    Booking entity added

--- odyssey_travel_backend/src/main/java/com/odyssey/entity/Booking.java
+++ odyssey_travel_backend/src/main/java/com/odyssey/entity/Booking.java
@@ -1,5 +1,27 @@
 package com.odyssey.entity;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "bookings")
 public class Booking {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+
+    private LocalDate travelDate;
+    private int travelers;
+    private String bookingStatus;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id", nullable = false)
+    private User client;
+
+    @ManyToOne
+    @JoinColumn(name = "package_id", nullable = false)
+    private TravelPackage travelPackage;
 }
+
