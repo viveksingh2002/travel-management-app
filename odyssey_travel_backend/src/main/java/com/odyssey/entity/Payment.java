a82e70954597acef75523fd721dcbfdc49f00918
Amruta <patilamrutak2003@gmail.com>
2026-01-24 18:28:29 +0530

    Payment entity added

--- odyssey_travel_backend/src/main/java/com/odyssey/entity/Payment.java
+++ odyssey_travel_backend/src/main/java/com/odyssey/entity/Payment.java
@@ -1,5 +1,19 @@
 package com.odyssey.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "payments")
 public class Payment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long paymentId;
+
+    private double amount;
+    private String paymentStatus;
+
+    @OneToOne
+    @JoinColumn(name = "booking_id", nullable = false)
+    private Booking booking;
 }
