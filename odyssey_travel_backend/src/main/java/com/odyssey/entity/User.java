208b955cf204752aea04d5df2ae4bfce5b4127d1
Amruta <patilamrutak2003@gmail.com>
2026-01-24 17:30:26 +0530

    User Entity added

--- odyssey_travel_backend/src/main/java/com/odyssey/entity/User.java
+++ odyssey_travel_backend/src/main/java/com/odyssey/entity/User.java
@@ -1,5 +1,29 @@
 package com.odyssey.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;   // CLIENT, AGENT, ADMIN
+
+    private boolean active; // approved or blocked
 }
+
+
